afd4dfd254f74485290aac9b50f88e9ce190a015
Davide <barbang.davide@gmail.com>
2021-10-13 18:30:51 +0200

    feat: draft of buttons

---
+++ components/ButtonGroup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="btn-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'CustomButtonGroup'
+})
+</script>
+
+<style scoped lang="scss">
+.btn-group {
+  @apply flex rounded-l-md rounded-r-md;
+  & >>> .btn:first-child {
+    @apply rounded-l-md;
+  }
+  & >>> .btn:last-child {
+    @apply rounded-r-md;
+  }
+}
+</style>
