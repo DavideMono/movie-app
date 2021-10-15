8b439562f2758deff628c1024f59803526265cd1
Dacker15 <barbang.davide@gmail.com>
2021-10-15 16:57:51 +0200

    perf(header): improve styles

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container mx-auto p-4 flex items-center">
-    <div class="flex items-center cursor-pointer">
+  <div class="container mx-auto p-4 flex flex-wrap gap-y-4 items-center">
+    <div class="flex items-center cursor-pointer" @click="onHome">
       <img class="w-8" src="/movie-icon.svg" alt="Movie Icon" />
       <span class="text-4xl font-bold">Movie Browser</span>
     </div>
     <div class="flex-1" />
     <input
-      class="border-2 px-2 py-1 text-2xl rounded-md focus:outline-none focus:border-blue-500"
+      class="border-2 px-2 py-1 text-2xl rounded-md focus:outline-none focus:border-blue-500 w-full md:w-auto"
       placeholder="Search by movie title"
       :value="value"
       @input="(e) => onUpdate(e.target.value)"
@@ -28,9 +28,10 @@ export default Vue.extend({
     },
     onDebouncedSearch: debounce((nextSearch: string, router: any) => {
       router.push({ path: `/search/${nextSearch}` })
-    }, 500)
+    }, 500),
+    onHome() {
+      this.$router.push({ path: '/' })
+    }
   }
 })
 </script>
-
-<style scoped></style>
